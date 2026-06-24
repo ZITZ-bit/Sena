37e6973f7b3b858726a2c773a861875ec6651b75
ZITZ <zitzcode@gmail.com>
2026-06-24 16:49:57 -0400

    Admin | Dashboard - Sidebar

---
+++ apps/admin/src/hooks/useSidebar.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const menuItems = [
+  {
+    icon: "bx-home-alt",
+    label: "Inicio"
+  },
+  {
+    icon: "bx-user-plus",
+    label: "Registrar Profesor"
+  },
+  {
+    icon: "bx-group",
+    label: "Ver Profesores"
+  },
+  {
+    icon: "bx-book-add",
+    label: "Registrar Estudiantes"
+  },
+  {
+    icon: "bx-id-card",
+    label: "Ver Estudiantes"
+  }
+];
+
+export function useSideBar() {
+
+  const [open, setOpen] = useState(false);
+
+  const toggleSideBar = () => {
+    setOpen(prev => !prev);
+  };
+
+  return {
+    open,
+    menuItems,
+    toggleSideBar
+  };
+
+}
