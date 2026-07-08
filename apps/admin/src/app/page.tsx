4a167cec7be26d3d3846ab1026bcd389f6547234
ZITZ <zitzcode@gmail.com>
2026-07-07 20:43:46 -0400

    Admin | Sidebar/Component

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,7 +1,21 @@
+import "../styles/Dashboard.css"
+
+import SideBar from "../components/SideBar";
+
 export default function Dashboard() {
   return (
     <>
+      <header>
+        <SideBar />
+      </header>
 
+      <main>
+        
+      </main>
+
+      <footer>
+
+      </footer>
     </>
   );
 }
