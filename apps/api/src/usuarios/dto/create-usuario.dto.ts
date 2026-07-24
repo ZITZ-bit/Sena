4f62d28c0e9ad5f303375b829fcd470a551fc1f4
ZITZ <zitzcode@gmail.com>
2026-07-23 23:19:22 -0400

    Api | Modulo usuarios

---
+++ apps/api/src/usuarios/dto/create-usuario.dto.ts
@@ -0,0 +1,11 @@
+import { IsString, Length, MinLength } from 'class-validator';
+
+export class CreateUsuarioDto {
+  @IsString()
+  @Length(7, 20)
+  cedula: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
