600624bef69bfe8753ef1326b28c38eb03566de7
ZITZ <zitzcode@gmail.com>
2026-08-04 20:17:49 -0400

    Monorepo | Prisma - env

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,12 +1,25 @@
+import { config } from 'dotenv';
+import { join } from 'node:path';
+
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
+const envPath = join(__dirname, '..', '..', 'prisma', '.env');
+config({ path: envPath });
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+
   constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (typeof connectionString !== 'string' || connectionString.length === 0) {
+      throw new Error('DATABASE_URL no está definida o no es una cadena válida');
+    }
+
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL!,
+      connectionString,
     });
 
     super({
