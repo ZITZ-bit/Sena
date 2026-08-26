b4b904a1a5833937af129f94c76bc97c74fa714a
ZITZ <zitzcode@gmail.com>
2026-08-25 21:19:17 -0400

    Web | Tabla - Profesor

---
+++ apps/admin/src/components/FormProf.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+/* Componentes De Next */
+
+import "boxicons/css/boxicons.min.css";
+
+/* Componentes De Next */
+
+
+/* Hooks */
+
+
+function FormProf() {
+  return (
+    <>
+      
+    </>
+  );
+
+}
+
+export default FormProf;
